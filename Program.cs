5308ba9d6a6b8c1c1234965b9c57833af9998e64
rutujajadhav00 <rutujajadhav413512@gmail.com>
2025-05-29 23:03:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 5;
+            static void Main(string[] args)
+            {
+                int count = 5;
+                for (int i = 1; i <= count; i++)
+                {
+
+                    for (int k = 1; k <= count - 1; k++)
+                    {
+                        Console.Write(" ");
+
+                    }
+                    for (int j = 1; j <= count; j++)
+                    {
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine();
+                }
+
+
+
+            }
+        }
+    }
+}
